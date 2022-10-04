f42bd773f044f312acaaa6b14a2202223e53b24a
Alex-Farias <alexfabreu02@gmail.com>
2022-10-04 09:52:22 -0300

    Aqui é um teste com o TextInput

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    return (
+        <>
+            //Aqui é uma view com propriedades vindas de uma herança
+            <View style={styles.container}>
+                <Text>Hello There!</Text>
+                <StatusBar style="auto" />
+            </View>
+            //Aqui é uma view com propiedades sem herança
+            <View style={{ flex: 1, justifyContent: "center", alignItems:"center" }} >
+                <Text>Im afraid because i get fired</Text>
+            </View>
+        </>
+        
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    container: {
+        flex: 1,
+        backgroundColor: 'RED',
+        alignItems: 'center',
+        justifyContent: 'center',
   },
 });
